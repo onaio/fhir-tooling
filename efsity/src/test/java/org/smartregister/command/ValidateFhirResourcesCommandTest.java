780ddf3bc9f9a2c0f3d9290571d92287ca1cecad
lincmba <lincolncmba@gmail.com>
2023-12-21 17:01:45 +0300

    Add Validate Fhir Resources Tests  fixes https://github.com/onaio/fhir-tooling/issues/93

---
+++ efsity/src/test/java/org/smartregister/command/ValidateFhirResourcesCommandTest.java
@@ -0,0 +1,47 @@
+package org.smartregister.command;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import net.jimblackler.jsonschemafriend.GenerationException;
+import net.jimblackler.jsonschemafriend.ValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.smartregister.domain.FctFile;
+import org.smartregister.util.FctUtils;
+
+import java.io.IOException;
+import ca.uhn.fhir.parser.DataFormatException;
+
+public class ValidateFhirResourcesCommandTest {
+
+  private ValidateFhirResourcesCommand validateFhirResourcesCommand;
+
+  @BeforeEach
+  void setUp() {
+    validateFhirResourcesCommand = new ValidateFhirResourcesCommand();
+  }
+  @Test
+  void testValidateResource() throws IOException, ValidationException, GenerationException {
+    // valid resource
+    String input = "src/test/resources/raw_questionnaire.json";
+    assertDoesNotThrow(() -> validateFhirResourcesCommand.validateFhirResources(input));
+    // invalid resource
+    String invalidInput = "src/test/resources/fhirConfigsJsonSchema.json";
+    assertThrows(DataFormatException.class, () -> validateFhirResourcesCommand.validateFhirResources(invalidInput));
+
+  }
+
+  @Test
+  void testValidateConfig() throws IOException, ValidationException, GenerationException {
+    // valid config
+    String input = "src/test/resources/profile_config.json";
+    FctFile inputFile = FctUtils.readFile(input);
+    validateFhirResourcesCommand.configSchema = "src/test/resources/fhirConfigsJsonSchema.json";
+    assertDoesNotThrow(() -> validateFhirResourcesCommand.validateConfig(inputFile));
+
+    // invalid config
+    String invalidInput = "src/test/resources/raw_questionnaire.json";
+    FctFile invalidInputFile = FctUtils.readFile(invalidInput);
+    assertThrows(ValidationException.class, () -> validateFhirResourcesCommand.validateConfig(invalidInputFile));
+  }
+}
