530e31fc4aeac347d6e7aeb5ed60960184c3e131
lincmba <lincolncmba@gmail.com>
2023-12-21 14:08:41 +0300

    Validate FHIR Resources before publishing.  fixes https://github.com/onaio/fhir-tooling/issues/87

--- efsity/src/main/java/org/smartregister/command/ValidateFhirResourcesCommand.java
+++ efsity/src/main/java/org/smartregister/command/ValidateFhirResourcesCommand.java
@@ -42,7 +42,7 @@ public class ValidateFhirResourcesCommand implements Runnable {
       names = {"-s", "--schema"},
       description = "configs schema"
   )
-  private String configSchema;
+  private static String configSchema;
 
   @Override
   public void run() {
@@ -55,7 +55,7 @@ public class ValidateFhirResourcesCommand implements Runnable {
     }
   }
 
-  private void validateFhirResources(String inputFilePath) throws IOException, ValidationException, GenerationException {
+  public static void validateFhirResources(String inputFilePath) throws IOException, ValidationException, GenerationException {
 
     long start = System.currentTimeMillis();
 
@@ -119,7 +119,7 @@ public class ValidateFhirResourcesCommand implements Runnable {
     }
   }
 
-  private int validateResource(FhirValidator validator, IBaseResource resource) {
+  private static int validateResource(FhirValidator validator, IBaseResource resource) {
 
     ValidationResult result = validator.validateWithResult(resource);
 
@@ -146,12 +146,12 @@ public class ValidateFhirResourcesCommand implements Runnable {
     }
   }
 
-  boolean isConfigFile(FctFile inputFile){
+  static boolean isConfigFile(FctFile inputFile){
     JSONObject resource = new JSONObject(inputFile.getContent());
     return resource.has("configType");
   }
 
-  int validateConfig(FctFile configFile) throws GenerationException, ValidationException {
+  static int validateConfig(FctFile configFile) throws GenerationException, ValidationException {
     SchemaStore schemaStore = new SchemaStore();
     Schema schema = schemaStore.loadSchema(new File(String.valueOf(Paths.get(configSchema))));
 
