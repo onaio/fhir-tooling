c13f98c71e94a494d082d242818efce913891037
Martin Ndegwa <mndegwa@ona.io>
2024-07-05 13:24:05 +0300

    Update TransformSupportServices class to match FHIRCore

--- efsity/src/main/kotlin/org/smartregister/external/TransformSupportServices.kt
+++ efsity/src/main/kotlin/org/smartregister/external/TransformSupportServices.kt
@@ -40,7 +40,7 @@ class TransformSupportServices constructor(private val simpleWorkerContext: Simp
   override fun createType(appInfo: Any, name: String): Base {
     return when (name) {
       "RiskAssessment_Prediction" -> RiskAssessmentPredictionComponent()
-      "Immunization_VaccinationProtocol" -> Immunization.ImmunizationProtocolAppliedComponent()
+      "Immunization_AppliedProtocol" -> Immunization.ImmunizationProtocolAppliedComponent()
       "Immunization_Reaction" -> Immunization.ImmunizationReactionComponent()
       "EpisodeOfCare_Diagnosis" -> EpisodeOfCare.DiagnosisComponent()
       "Encounter_Diagnosis" -> Encounter.DiagnosisComponent()
