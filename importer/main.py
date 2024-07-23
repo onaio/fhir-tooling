3845511167ef16c9b4428d57b4571d62db269e41
Wambere <janette@wambere.com>
2024-07-23 16:02:21 +0300

    Fix bug in product list resource (#233)
    * Fix bug: unexpected argument 'defaultgroups'
    
    * Fix string-int comparison and update documentation

--- importer/main.py
+++ importer/main.py
@@ -1818,7 +1818,7 @@ LOGGING = {
 @click.option("--setup", required=False)
 @click.option("--group", required=False)
 @click.option("--roles_max", required=False, default=500)
-@click.option("--defaultgroups", required=False, default=False)
+@click.option("--default_groups", required=False, default=False)
 @click.option("--cascade_delete", required=False, default=False)
 @click.option("--only_response", required=False)
 @click.option(
@@ -1992,7 +1992,7 @@ def main(
                     list_resource_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, csv_file))
 
                 current_version = get_resource(list_resource_id, "List")
-                method = "create" if current_version == 0 else "update"
+                method = "create" if current_version == str(0) else "update"
                 resource = [["Supply Inventory List", "current", method, list_resource_id]]
                 result_payload = build_payload(
                     "List", resource, "json_payloads/product_list_payload.json")
