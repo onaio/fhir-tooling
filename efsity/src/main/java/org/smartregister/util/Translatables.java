9197d6554f1d51be2d1da42ee54f0fafe8cc4f4e
hilary egesa <hilpitome@gmail.com>
2024-07-24 10:53:14 +0300

    setup snakeyaml

---
+++ efsity/src/main/java/org/smartregister/util/Translatables.java
@@ -0,0 +1,26 @@
+package org.smartregister.util;
+
+import java.util.List;
+
+public class Translatables {
+  private List<String> questionnaireTranslatables;
+  private List<String> configTranslatables;
+
+  // Getters and Setters
+
+  public List<String> getQuestionnaireTranslatables() {
+    return questionnaireTranslatables;
+  }
+
+  public void setQuestionnaireTranslatables(List<String> questionnaireTranslatables) {
+    this.questionnaireTranslatables = questionnaireTranslatables;
+  }
+
+  public List<String> getConfigTranslatables() {
+    return configTranslatables;
+  }
+
+  public void setConfigTranslatables(List<String> configTranslatables) {
+    this.configTranslatables = configTranslatables;
+  }
+}
