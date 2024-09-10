98e4b629f5ffb87b56511b7bfb81e84b876d16d5
sharon2719 <otiennosharon@gmail.com>
2024-09-10 18:44:46 +0300

    Add structueMap validation against QRs

--- efsity/src/main/java/org/smartregister/command/ValidateStructureMapCommand.java
+++ efsity/src/main/java/org/smartregister/command/ValidateStructureMapCommand.java
@@ -31,31 +31,29 @@ public class ValidateStructureMapCommand implements Runnable {
       defaultValue = "false")
   boolean validate;
 
+  @CommandLine.Option(
+      names = {"-sm", "--structure-map"},
+      description = "structure map file path",
+      required = true)
+  String structureMapFilePath;
+
   @Override
   public void run() {
     if (inputPath != null) {
       try {
-        validateStructureMap(inputPath, validate);
+        validateStructureMap(inputPath, validate, structureMapFilePath);
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
     }
   }
 
-  void validateStructureMap(String inputFilePath, boolean validate) throws IOException {
+  void validateStructureMap(String inputFilePath, boolean validate, String structureMapFilePath)
+      throws IOException {
     long start = System.currentTimeMillis();
+
     FctUtils.printInfo("Starting structureMap validation");
     FctUtils.printInfo(String.format("Input file path \u001b[35m%s\u001b[0m", inputFilePath));
-    // Define the directory name for generated resources
-    String directoryName = "generatedResources";
-
-    // Create the directory if it doesn't exist
-    Path generatedResourcesPath = Paths.get(directoryName);
-    if (!Files.exists(generatedResourcesPath)) {
-      Files.createDirectory(generatedResourcesPath);
-      FctUtils.printInfo(
-          "Created directory: " + generatedResourcesPath.toAbsolutePath().toString());
-    }
 
     ArrayList<String> questionnaires = getResourceFiles(inputFilePath);
     for (String questionnairePath : questionnaires) {
@@ -70,6 +68,17 @@ public class ValidateStructureMapCommand implements Runnable {
         }
       }
 
+      // Define the directory name for generated resources
+      String directoryName = "generatedResources";
+
+      // Create the directory if it doesn't exist
+      Path generatedResourcesPath = Paths.get(directoryName);
+      if (!Files.exists(generatedResourcesPath)) {
+        Files.createDirectory(generatedResourcesPath);
+        FctUtils.printInfo(
+            "Created directory: " + generatedResourcesPath.toAbsolutePath().toString());
+      }
+
       // Generate QuestionnaireResponse
       QuestionnaireResponseGeneratorCommand.generateResponse(
           questionnairePath,
@@ -80,6 +89,20 @@ public class ValidateStructureMapCommand implements Runnable {
           "",
           "",
           "");
+
+      // Extract Resources using the StructureMap and the generated QuestionnaireResponse
+      String generatedQuestionnaireResponsePath =
+          generatedResourcesPath.toString()
+              + File.separator
+              + Paths.get(questionnairePath)
+                  .getFileName()
+                  .toString()
+                  .replace(".json", "-response.json");
+
+      StructureMapExtractResourcesCommand.extractResource(
+          generatedQuestionnaireResponsePath,
+          structureMapFilePath,
+          generatedResourcesPath.toString());
     }
     FctUtils.printCompletedInDuration(start);
   }
