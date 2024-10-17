ada2b013dec9db417f109318458eda1c00fdb445
Sharon Akinyi <79141719+sharon2719@users.noreply.github.com>
2024-10-17 21:18:19 +0300

    Handle unsupported datatypes (#280)
    * Enhance determineFhirDataType function
    
    * Fix determineFhirDataType
    
    * Resolve conflicts
    
    * Test transform support services. (#282)
    
    Signed-off-by: Lentumunai-Mark <lentumunai.mark@students.jkuat.ac.ke>
    Co-authored-by: Peter Lubell-Doughtie <peter@ona.io>
    
    * -Add new OpenSRP web roles (#286)
    
    * Update README.md (#287)
    
    * Enhance determineFhirDataType function
    
    * Resolve conflicts
    
    * Resolve conflicts
    
    ---------
    
    Signed-off-by: Lentumunai-Mark <lentumunai.mark@students.jkuat.ac.ke>
    Co-authored-by: Peter Lubell-Doughtie <peter@ona.io>
    Co-authored-by: Lentumunai Mark <90028422+Lentumunai-Mark@users.noreply.github.com>
    Co-authored-by: Benjamin Mwalimu <dubdabasoduba@gmail.com>

--- sm-gen/src/main/kotlin/org.smartregister.fhir.structuremaptool/Utils.kt
+++ sm-gen/src/main/kotlin/org.smartregister.fhir.structuremaptool/Utils.kt
@@ -276,21 +276,21 @@ class Group(
         if (answerExpression != null) {
           if (
             answerExpression.isNotEmpty() &&
-              answerExpression.isNotBlank() &&
-              answerExpression != "''"
+            answerExpression.isNotBlank() &&
+            answerExpression != "''"
           ) {
             val propertyType = inferType(instruction!!.fullPropertyPath())
             val answerType = answerExpression.getAnswerType(questionnaireResponse)
 
             if (
               propertyType != "Type" &&
-                answerType != propertyType &&
-                propertyType
-                  ?.canHandleConversion(
-                    answerType ?: "",
-                  )
-                  ?.not() == true &&
-                answerExpression.startsWith("evaluate")
+              answerType != propertyType &&
+              propertyType
+                ?.canHandleConversion(
+                  answerType ?: "",
+                )
+                ?.not() == true &&
+              answerExpression.startsWith("evaluate")
             ) {
               println(
                 "Failed type matching --> ${instruction!!.fullPropertyPath()} of type $answerType != $propertyType"
@@ -435,7 +435,7 @@ private fun String.getType(questionnaireResponse: QuestionnaireResponse): String
 internal val fhirPathEngine: FHIRPathEngine =
   with(FhirContext.forCached(FhirVersionEnum.R4)) {
     FHIRPathEngine(HapiWorkerContext(this, this.validationSupport)).apply {
-      hostServices = FHIRPathEngineHostServices
+      hostServices = FhirPathEngineHostServices
     }
   }
 
